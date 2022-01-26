fe644629c7b8a2a6f55e705211b5fced9d4d665b
GvMazzon25 <gvmazzon25@gmail.com>
2022-01-26 16:59:21 +0100

    home

---
+++ resources/views/partials/footer.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <footer>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos tempora accusantium odit, exercitationem harum unde quas culpa, veritatis aut quidem reiciendis nihil, explicabo ipsum ea repellendus assumenda eveniet illo dolorem?</p>
+    </footer>
+</body>
+</html>
