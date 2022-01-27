d9b575a13dd76f2f143dc7f0627cc143c745229b
GvMazzon25 <gvmazzon25@gmail.com>
2022-01-27 17:21:48 +0100

    prova

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -9,7 +9,24 @@
 <body>
     <nav class="navbar navbar-light bg-light">
         <div class="container-fluid">
-          <span class="navbar-brand mb-0 h1"><a href="{{ route('home') }}">Comics</a></span>
+          <span class="navbar-brand mb-0 h1"><a href="">Comics</a></span>
+          <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+              <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="{{ route('home') }}">Home</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="{{ route('resource') }}">Link</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link disabled">Disabled</a>
+              </li>
+            </ul>
+            <form class="d-flex">
+              <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+              <button class="btn btn-outline-success" type="submit">Search</button>
+            </form>
+          </div>
         </div>
       </nav>
 </body>
